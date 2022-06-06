69b4d3871606decce1772191f137a2383921e69e
thunderclaw09 <elisalam07@gmail.com>
2022-06-06 11:59:22 -0400

    made first version

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <Text>Cammy and Pebby are cute!</Text>
+      <Image source={logo} style={{ width: 305, height: 300 }} /> 
+      {/* <Text>Hello world!</Text> */}
+      <Text style={{color: '#089', fontSize: 30}}>Cammy and Pebby are cute!</Text>
+      <Text style={{color: '#888', fontSize: 18}}>So is the screaming hamster above.</Text>
+      <Text style={{color: '#889', fontSize: 18}}>            </Text>
+
+
+      <TouchableOpacity onPress={() => alert('Yay! You pressed me!')} style={styles.button}>
+        <Text style={styles.buttonText}>button</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +28,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
